e988e1d6a73be776cf62854662a1d743c780f594
dhanraj12i <dhanraj12061998@gmail.com>
2024-08-28 20:37:29 +0530

    sonar error resolved

--- src/App.tsx
+++ src/App.tsx
@@ -6,19 +6,17 @@ import RequiredAuth from "./hooks/authCheck";
 
 const App = () => {
   return (
-    <>
-      <BrowserRouter>
-        <Routes>
-          <Route element={<RequiredAuth />}>
-            <Route path="/Login" element={<Login />} />
-          </Route>
-          <Route element={<RequiredAuth />}>
-            <Route path="/" element={<HomePage />} />
-            <Route path="/home" element={<HomePage />} />
-          </Route>
-        </Routes>
-      </BrowserRouter>
-    </>
+    <BrowserRouter>
+      <Routes>
+        <Route element={<RequiredAuth />}>
+          <Route path="/Login" element={<Login />} />
+        </Route>
+        <Route element={<RequiredAuth />}>
+          <Route path="/" element={<HomePage />} />
+          <Route path="/home" element={<HomePage />} />
+        </Route>
+      </Routes>
+    </BrowserRouter>
   );
 };
 
