711004121192ee221e14ccd6806f321af76ba5b8
dhanraj12i <dhanraj12061998@gmail.com>
2024-08-30 09:40:07 +0530

    test config setup

---
+++ src/tests/components/Login.spec.tsx
@@ -0,0 +1,22 @@
+import React from "react";
+import { render, screen } from "@testing-library/react";
+
+import Login from "../../componenets/Login";
+
+describe("Login component tests", () => {
+  beforeEach(() => {
+    // write someting before each test
+  });
+
+  afterEach(() => {
+    // write someting after each test
+  });
+
+  it("Renders correctly initial document", async () => {
+    /* first we visit /login and test if the string in the element with class "login-label"  has"Please Log In" is there */
+    render(<Login />);
+    const loginLabel = screen.getByText("Please Log In");
+
+    expect(loginLabel).toBeInTheDocument();
+  });
+});
