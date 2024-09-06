9f00e90e21cc5ed819e4e3421b5a89672ac6417c
dhanraj12i <dhanraj12061998@gmail.com>
2024-09-07 02:19:14 +0530

    build created test

--- vite.config.ts
+++ vite.config.ts
@@ -4,5 +4,20 @@ import react from "@vitejs/plugin-react-swc";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "/vite-deploy-demo/",
+  build: {
+    outDir: "dist",
+    target: "esnext",
+    rollupOptions: {
+      output: {
+        entryFileNames: "[name].js",
+        chunkFileNames: "[name].js",
+        assetFileNames: "[name].[ext]",
+      },
+    },
+  },
+  resolve: {
+    alias: {
+      "@": "/src",
+    },
+  },
 });
